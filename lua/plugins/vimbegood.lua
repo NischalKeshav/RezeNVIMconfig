b926a8db1b0382f26fd14aca4993ffb930fb2a33
Nischal Keshav <nischalkeshav.official@gmail.com>
2025-11-03 19:03:15 -0600

    removed vim be good

--- lua/plugins/vimbegood.lua
+++
@@ -1,7 +0,0 @@
-    return {
-      "thePrimeagen/vim-be-good",
-      cmd = "VimBeGood", -- This ensures the plugin is only loaded when the command is used
-      config = function()
-        require("VimBeGood").setup {} -- Basic setup, customize as needed
-      end,
-    }
